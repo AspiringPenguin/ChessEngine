e75b021d136a8213623888d7bcbc739a4f135898
AspiringPenguin <253924359+AspiringPenguin@users.noreply.github.com>
2025-12-30 15:13:57 +0000

    Started work on move encoding

--- ChessEngine/moves.cpp
+++ ChessEngine/moves.cpp
@@ -4,4 +4,32 @@ namespace moves {
 	move encodeMove(square from, square to, piece capture, bool promote, bool isCapture, bool special1, bool special2) {
 		return from | (to << 6) | (capture << 12) | (promote << 16) | (isCapture << 17) | (special1 << 18) | (special2 << 19);
 	}
+
+	square getFrom(move m){
+		return square(m & 0x3F);
+	}
+
+	square getTo(move m){
+		return square((m & 0xFC0) >> 6);
+	}
+
+	piece getCapture(move m){
+		return piece((m & 0xF000) >> 12);
+	}
+
+	bool getPromote(move m){
+		return false;
+	}
+
+	bool getCaptureFlag(move m){
+		return false;
+	}
+
+	bool getSpecial1(move m){
+		return false;
+	}
+
+	bool getSpecial2(move m){
+		return false;
+	}
 }
--- ChessEngine/moves.h
+++ ChessEngine/moves.h
@@ -3,4 +3,12 @@
 
 namespace moves {
 	move encodeMove(square from, square to, piece capture, bool promote, bool isCapture, bool special1, bool special2);
+
+	square getFrom(move m);
+	square getTo(move m);
+	piece getCapture(move m);
+	bool getPromote(move m);
+	bool getCaptureFlag(move m);
+	bool getSpecial1(move m);
+	bool getSpecial2(move m);
 }
