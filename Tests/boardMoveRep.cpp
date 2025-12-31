7e27d6261b89ac2ae2a52786387fbf9e4122270f
AspiringPenguin <253924359+AspiringPenguin@users.noreply.github.com>
2025-12-31 19:41:16 +0000

    Added more move rep tests

--- Tests/boardMoveRep.cpp
+++ Tests/boardMoveRep.cpp
@@ -28,5 +28,37 @@ namespace boardMoveRep
 			move m = moves::encodeMove(H8, A1, bKing, bKing,  true, true, true, true, true, true, true, true);
 			Assert::IsTrue(moves::getTo(m) == A1);
 		}
+
+		TEST_METHOD(CheckPiece1) { //Check the right section is being extracted
+			move m = moves::encodeMove(A1, A1, bKing, nullPiece, false, false, false, false, false, false, false, false);
+			Assert::IsTrue(moves::getPiece(m) == bKing);
+		}
+
+		TEST_METHOD(CheckPiece2) { //Check nothing overwrites
+			move m = moves::encodeMove(H8, H8, nullPiece, bKing, true, true, true, true, true, true, true, true);
+			Assert::IsTrue(moves::getPiece(m) == nullPiece);
+		}
+
+		TEST_METHOD(CheckCapture1) { //Check the right section is being extracted
+			move m = moves::encodeMove(A1, A1, nullPiece, bKing, false, false, false, false, false, false, false, false);
+			Assert::IsTrue(moves::getCapture(m) == bKing);
+		}
+
+		TEST_METHOD(CheckCapture2) { //Check nothing overwrites
+			move m = moves::encodeMove(H8, H8, bKing, nullPiece, true, true, true, true, true, true, true, true);
+			Assert::IsTrue(moves::getCapture(m) == nullPiece);
+		}
+
+		//bool promote, bool special1, bool special2, bool enPassant, bool wkc, bool wqc, bool bkc, bool bqc
+
+		TEST_METHOD(CheckPromoteFlag1) { //Check the right section is being extracted
+			move m = moves::encodeMove(A1, A1, nullPiece, nullPiece, true, false, false, false, false, false, false, false);
+			Assert::IsTrue(moves::getPromoteFlag(m) == true);
+		}
+
+		TEST_METHOD(CheckPromoteFlag2) { //Check nothing overwrites
+			move m = moves::encodeMove(H8, H8, bKing, bKing, false, true, true, true, true, true, true, true);
+			Assert::IsTrue(moves::getPromoteFlag(m) == false);
+		}
 	};
 }
