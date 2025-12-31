ad56fc6983b9fd28589ebfc389458819562029e1
AspiringPenguin <253924359+AspiringPenguin@users.noreply.github.com>
2025-12-31 18:47:18 +0000

    Improved move setup to suit my needs

--- ChessEngine/moves.cpp
+++ ChessEngine/moves.cpp
@@ -1,9 +1,13 @@
 #include "moves.h"
 
 namespace moves {
-	move encodeMove(square from, square to, piece capture, bool promote, bool isCapture, bool special1, bool special2) {
-		return from | (to << 6) | (capture << 12) | (promote << 16) | (isCapture << 17) | (special1 << 18) | (special2 << 19);
+	move encodeMove(square from, square to, piece capture, bool promote, bool special1, bool special2, bool enPassant) {
+		return from | (to << 6) | (capture << 12) | (promote << 16) | (special1 << 17) | (special2 << 18) | (enPassant << 19);
 	}
+
+	//Special 1 = double pawn push
+	//Special 2 = king castle
+	//Special 1 and 2 = queen castle
 
 	square getFrom(const move& m){
 		return square(m & 0x3F);
@@ -28,15 +32,15 @@ namespace moves {
 		return piece(flag1 ? (flag2 ? wQueen : wRook) : (flag2 ? wBishop : wKnight) | (color << 3));
 	}
 
-	bool getCaptureFlag(const move& m){
-		return (m & 0x20000) >> 14;
-	}
-
 	bool getSpecial1(const move& m){
-		return (m & 0x40000) >> 15;
+		return (m & 0x20000) >> 17;
 	}
 
 	bool getSpecial2(const move& m){
-		return (m & 0x80000) >> 16;
+		return (m & 0x40000) >> 18;
+	}
+
+	bool getEnPassant(const move& m) {
+		return (m & 0x80000) >> 19;
 	}
 }
--- ChessEngine/moves.h
+++ ChessEngine/moves.h
@@ -2,14 +2,14 @@
 #include "core.h"
 
 namespace moves {
-	move encodeMove(square from, square to, piece capture, bool promote, bool isCapture, bool special1, bool special2);
+	move encodeMove(square from, square to, piece capture, bool promote, bool special1, bool special2, bool enPassant);
 
 	square getFrom(const move& m);
 	square getTo(const move& m);
 	piece getCapture(const move& m);
 	bool getPromoteFlag(const move& m);
 	piece getPromote(const move& m, const color& color); //Interpret special flags
-	bool getCaptureFlag(const move& m);
 	bool getSpecial1(const move& m);
 	bool getSpecial2(const move& m);
+	bool getEnPassant(const move& m);
 }
