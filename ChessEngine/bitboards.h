6dc423fef2280160c8a622d6ec58fd5f6e50bc0a
AspiringPenguin <253924359+AspiringPenguin@users.noreply.github.com>
2026-01-08 19:09:19 +0000

    Added main and opposite diagonal bitboards

--- ChessEngine/bitboards.h
+++ ChessEngine/bitboards.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <array>
 #include "core.h"
 
 //For helper functions with bitboards
@@ -19,4 +20,20 @@ namespace bitboards {
 	constexpr U64 rank6 = 0x0000FF0000000000;
 	constexpr U64 rank7 = 0x00FF000000000000;
 	constexpr U64 rank8 = 0xFF00000000000000;
+
+	static std::array<U64, 8> ranks = {rank1, rank2, rank3, rank4, rank5, rank6, rank7, rank8};
+
+	constexpr U64 AFile = 0x0101010101010101;
+	constexpr U64 BFile = 0x0202020202020202;
+	constexpr U64 CFile = 0x0404040404040404;
+	constexpr U64 DFile = 0x0808080808080808;
+	constexpr U64 EFile = 0x1010101010101010;
+	constexpr U64 FFile = 0x2020202020202020;
+	constexpr U64 GFile = 0x4040404040404040;
+	constexpr U64 HFile = 0x8080808080808080;
+
+	constexpr U64 mainDiag = 0x8040201008040201;
+	constexpr U64 oppoDiag = 0x0102040810204080;
+
+	static std::array<U64, 8> files = { AFile, BFile, CFile, DFile, EFile, FFile, GFile, HFile };
 }
