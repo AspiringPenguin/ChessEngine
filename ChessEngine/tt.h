b0b2b1241a81851f4a29999105e129021864990f
AspiringPenguin <253924359+AspiringPenguin@users.noreply.github.com>
2026-01-16 18:02:47 +0000

    Added outline for stuff in tt.cpp. Also added structs and enums, as well as fiddling to get a tt entry into 8 bytes

--- ChessEngine/tt.h
+++ ChessEngine/tt.h
@@ -1,5 +1,26 @@
 #pragma once
+#include "core.h"
+#include <tuple>
 
 namespace tt {
+	enum ttResult {
+		ttScore, ttMove, ttNull
+	};
 
+	enum ttEntryType : short {
+		exact, lowerBound, upperBound
+	};
+
+	#pragma pack(push)
+	#pragma pack(2)
+	struct ttEntry {
+		move m;
+		short score;
+		ttEntryType type;
+	};
+	#pragma pack(pop)
+
+	std::tuple<ttResult, int> ttProbe(const U64& zobrist, const int& alpha, const int& beta, const int& depth);
+
+	void ttStore(const int& eval, const move& move, const int& depth); //Only possibly store
 }
