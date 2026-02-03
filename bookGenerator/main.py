3d9956e79c2530a71e9302510be16316807e40c4
AspiringPenguin <253924359+AspiringPenguin@users.noreply.github.com>
2026-02-03 17:36:30 +0000

    Modified the book generator to use a dictionary rather than a huge array to store all the moves in a position

--- bookGenerator/main.py
+++ bookGenerator/main.py
@@ -4,7 +4,7 @@ import chess.pgn
 import chess.polyglot
 import os
 
-positions = {} #Hash to list of moves
+positions = {} #Hash to dictionary of moves and frequencies
 
 def processNode(game : chess.GameNode | NoneType, zHash : int | NoneType):
     if game == None:
@@ -12,11 +12,14 @@ def processNode(game : chess.GameNode | NoneType, zHash : int | NoneType):
 
     if zHash:
         if zHash in positions:
-            positions[zHash].append(game.move)
+            if game.move in positions[zHash]:
+                positions[zHash][game.move] += 1
+            else:
+                positions[zHash][game.move] = 1
         else:
-            positions[zHash] = [game.move]
+            positions[zHash] = {game.move: 1}
 
-    if game.ply() > 10:
+    if game.ply() > 16:
         return
 
     zHash = chess.polyglot.zobrist_hash(game.board())
@@ -43,26 +46,26 @@ except KeyboardInterrupt:
 
 with open("book.txt", mode="w") as f:
     for zHash in positions.keys():
-        occurences = len(positions[zHash])
+        occurences = 0
+        for key in positions[zHash].keys():
+            occurences += positions[zHash][key]
 
         if zHash == 0x463b96181691fc9c: #Start position
-            minCount = int(occurences * 0.01)
-        elif zHash in [0x823c9b50fd114196, 0x830eb9b20758d1de, 0x9d5f7aee7e779da1, 0xca18093c559e579b]: #After 1. e4, d4, Nf3, c4 - black can play many different options here - don't suppress
-            minCount = int(occurences * 0.10)
+            minCount = int(occurences * 0.1)
         else:
             minCount = max(int(occurences * 0.3), 10)
 
         first = True
         
-        for move in sorted(set(positions[zHash]), key=lambda x: positions[zHash].count(x), reverse=True): #For each move in that position
-            if positions[zHash].count(move) < minCount: #Ignore rare moves
-                break
+        for move in positions[zHash].keys(): #For each move in that position
+            if positions[zHash][move] < minCount: #Ignore rare moves
+                continue
             
             if first:
                 f.write(str(zHash))
                 first = False
             
-            f.write(";" + move.uci() + "," + str(positions[zHash].count(move)))
+            f.write(";" + move.uci() + "," + str(positions[zHash][move]))
 
         if not first: #ie a move has been written
             f.write("\n")
