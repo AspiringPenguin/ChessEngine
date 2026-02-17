d6e7147386e05af61ff9ffb165bdffc85b94e45f
AspiringPenguin <253924359+AspiringPenguin@users.noreply.github.com>
2026-02-17 08:40:15 +0000

    Fixed a bug with checking for checkmates involving pawns and kings on the endge of the board

--- ChessEngine/position.cpp
+++ ChessEngine/position.cpp
@@ -1551,7 +1551,7 @@ bool Position::inCheck() {
 	square kingPos = square(SquareOf(kingBB));
 
 	//Check for each enemy piece type here
-	bool pawn = ((toMove == white) ? (((kingBB & ~bitboards::HFile) << 7) | ((kingBB & ~bitboards::AFile) << 9)) : (((kingBB & ~bitboards::HFile) >> 7) | ((kingBB & ~bitboards::AFile) >> 9))) & bitboards[wPawn + ((1 - toMove) << 3)];
+	bool pawn = ((toMove == black) ? (((kingBB & ~bitboards::HFile) >> 7) | ((kingBB & ~bitboards::AFile) >> 9)) : (((kingBB & ~bitboards::AFile) << 7) | ((kingBB & ~bitboards::HFile) << 9))) & bitboards[wPawn + ((1 - toMove) << 3)];
 
 	bool knight = moveGen::knightLookup[kingPos] & bitboards[wKnight + ((1 - toMove) << 3)];
 
